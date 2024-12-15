e8b0a7cc0142ee9505c3af0695e73fa85bdbf1f1
Rose <snowydg@proton.me>
2024-12-14 20:00:52 -0600

    matchmaking

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Elysia } from "elysia";
 import { cors } from '@elysiajs/cors'
-
+import fs from 'fs';
+import path from 'path';
+const ASSETS_FOLDER = './assets'; // Path to the assets folder
 
 const app = new Elysia()
     .get('/assets', ({ query }) => {
@@ -20,6 +22,60 @@ const app = new Elysia()
             }
         };
     })
+    .get('/volume', () => {
+
+    })
+    .post('/matchmaking-log', ({body}) => {
+      console.log(body)
+    })
+    .post('/kill', ({ query }) => {
+        const { username, auth } = query;
+
+        if (!username || !auth) {
+            return {
+                error: "Missing required query parameters: username and auth"
+            };
+        }
+
+        return {
+            message: "Query parameters received successfully",
+            data: {
+                username,
+                auth
+            }
+        };
+    })
+    .post('/launch-offline', () => {
+      return {
+        message: "hai"
+      }
+    })
+    .post('/region-ping',() => {
+        return {
+            message: "Query parameters received successfully",
+            data: {
+            }
+        };
+    })
+    .post('/fetch-assets', () => {
+      try {
+          const files = fs.readdirSync(ASSETS_FOLDER);
+  
+          const assets = files.map((file) => {
+              const filePath = path.join(ASSETS_FOLDER, file);
+              const stats = fs.statSync(filePath);
+              return {
+                  name: file,
+                  size: `${(stats.size / (1024 * 1024)).toFixed(1)} MB`
+              };
+          });
+  
+          return assets; 
+      } catch (error) {
+          console.error("Error fetching assets:", error);
+          return { error: "Failed to fetch assets" };
+      }
+  })
     .use(cors())
     .listen(8080);
 
